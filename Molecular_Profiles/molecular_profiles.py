cf5dd363e3340baae2e6553e75edefe3c8068de4
Matteo Valerio <matteo.valerio@live.it>
2023-11-02 14:21:22 +0100

    molecular profiles

--- Molecular_Profiles/molecular_profiles.py
+++ Molecular_Profiles/molecular_profiles.py
@@ -1,11 +1,12 @@
 import requests
 import pandas as pd
-from pyBioGate.config import base_url
+from config import base_url
+from aux_funcs import check_response
 
 ######################
 # Molecular Profiles #
 ######################
-def get_all_molecular_profiles(self, direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy="datatype"):
+def get_all_molecular_profiles(direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy="datatype"):
     """
     Get all molecular profiles.
     :param direction: Direction of the sort.
@@ -30,8 +31,8 @@ def get_all_molecular_profiles(self, direction="ASC", pageNumber=0, pageSize=100
         - "name": Sort by name.
         - "showProfileInAnalysisTab": Sort by profile visibility.
     :type sortBy: str, optional, default: "datatype"
-    :returns: List of molecular profiles.
-    :rtype: list[dict]
+    :returns: A DataFrame containing molecular profiles.
+    :rtype: pandas.DataFrame
     """
     endpoint = "/molecular-profiles"
     params = {
@@ -41,51 +42,54 @@ def get_all_molecular_profiles(self, direction="ASC", pageNumber=0, pageSize=100
         "projection": projection,
         "sortBy": sortBy
     }
-    response = requests.get(f"{self.base_url}{endpoint}", params=params)
-    if response.status_code == 200:
-        return pd.DataFrame(response.json())
-    else:
-        raise Exception(f"Failed to get all molecular profiles. Status code: {response.status_code}")
-def fetch_molecular_profiles(self, molecular_profile_filter, projection="SUMMARY"):
+
+    response = requests.get(f"{base_url}{endpoint}", params=params)
+    return check_response(response, "Failed to get all molecular profiles.")
+
+def get_molecular_profile(molecular_profile_id):
+    """
+    Get molecular profile.
+    :param molecular_profile_id: Molecular Profile ID, e.g., "acc_tcga_mutations".
+    :type molecular_profile_id: str
+    :returns: A DataFrame containing molecular profile information.
+    :rtype: pandas.DataFrame
+    """
+    endpoint = f"/molecular-profiles/{molecular_profile_id}"
+
+    response = requests.get(f"{base_url}{endpoint}")
+    return check_response(response, "Failed to get molecular profile.")
+    
+def fetch_molecular_profiles(molecular_profile_ids = None, study_ids = None, projection="SUMMARY"):
     """
     Fetch molecular profiles.
-    :param molecular_profile_filter: List of Molecular Profile IDs or List of Study IDs.
-    :type molecular_profile_filter: list[str]
+    :param molecular_profile_ids: List of Molecular Profile IDs, e.g.,
+    :type molecular_profile_ids: list of str
+    :param study_ids: List of Study IDs, e.g., .
+    :type study_ids: list of str
     :param projection: Level of detail of the response.
         - "DETAILED": Detailed information.
         - "ID": Information with only IDs.
         - "META": Metadata information.
         - "SUMMARY": Summary information (default).
     :type projection: str, optional, default: "SUMMARY"
-    :returns: List of fetched molecular profiles.
-    :rtype: list[dict]
+    :returns: A DataFrame containing fetched molecular profiles.
+    :rtype: pandas.DataFrame
     """
     endpoint = "/molecular-profiles/fetch"
     params = {"projection": projection}
-    response = requests.post(f"{self.base_url}{endpoint}", json=molecular_profile_filter, params=params, headers={'Content-Type': 'application/json'})
-    if response.status_code == 200:
-        return pd.DataFrame(response.json())
-    else:
-        raise Exception(f"Failed to fetch molecular profiles. Status code: {response.status_code}")
-def get_molecular_profile(self, molecular_profile_id):
-    """
-    Get molecular profile.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "acc_tcga_mutations".
-    :type molecular_profile_id: str
-    :returns: Molecular profile information.
-    :rtype: dict
-    """
-    endpoint = f"/molecular-profiles/{molecular_profile_id}"
-    response = requests.get(f"{self.base_url}{endpoint}")
-    if response.status_code == 200:
-        return pd.DataFrame(response.json())
-    else:
-        raise Exception(f"Failed to get molecular profile. Status code: {response.status_code}")
-        
-######################
-# Molecular Profiles #
-######################
-def get_all_molecular_profiles_in_study(self, study_id, direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy="datatype"):
+
+    molecular_profile_filter = {}
+
+    if molecular_profile_ids:
+        molecular_profile_filter['molecularProfileIds'] = molecular_profile_ids
+
+    if study_ids:
+        molecular_profile_filter['studyIds'] = study_ids
+    
+    response = requests.post(f"{base_url}{endpoint}", json=molecular_profile_filter, params=params)
+    return check_response(response, "Failed to fetch molecular profiles.")
+    
+def get_all_molecular_profiles_in_study(study_id, direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy="datatype"):
     """
     Get all molecular profiles in a study.
     :param study_id: Study ID, e.g., "acc_tcga".
@@ -113,8 +117,8 @@ def get_all_molecular_profiles_in_study(self, study_id, direction="ASC", pageNum
         - "name"
         - "showProfileInAnalysisTab"
     :type sortBy: str, optional, default: "datatype"
-    :returns: List of molecular profiles in the specified study.
-    :rtype: list[dict]
+    :returns: A DataFrame containing molecular profiles in the specified study.
+    :rtype: pandas.DataFrame
     """
     endpoint = f"/studies/{study_id}/molecular-profiles"
     params = {
@@ -124,8 +128,6 @@ def get_all_molecular_profiles_in_study(self, study_id, direction="ASC", pageNum
         "projection": projection,
         "sortBy": sortBy
     }
-    response = requests.get(f"{self.base_url}{endpoint}", params=params)
-    if response.status_code == 200:
-        return pd.DataFrame(response.json())
-    else:
-        raise Exception(f"Failed to get molecular profiles in the specified study. Status code: {response.status_code}")
+
+    response = requests.get(f"{base_url}{endpoint}", params=params)
+    return check_response(response, "Failed to get molecular profiles in the specified study.")
