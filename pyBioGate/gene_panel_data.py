0f7a829bba693a017ee4d5c15f7694709361d155
Matteo Valerio <matteo.valerio@live.it>
2023-11-07 13:53:35 +0100

    doc string format updated

--- pyBioGate/gene_panel_data.py
+++ pyBioGate/gene_panel_data.py
@@ -1,19 +1,18 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ###################
 # Gene Panel Data #
 ###################
 def fetch_gene_panel_data(molecular_profile_ids=None, molecular_prof_sample_ids=None):
     """
-    Fetch gene panel data from BioPortal.
-    :param molecular_profile_ids: List of Molecular Profile IDs, e.g., ["brca_tcga_gistic", "brca_tcga_mutations", "acc_tcga_gistic"].
-    :type molecular_profile_ids: list of str
-    :param molecular_prof_sample_ids: List of Molecular Profile ID and Sample IDs pairs.
-    :type molecular_prof_sample_ids: list of dict
-        Each dict should have the following format:
+    Fetch gene panel data from BioPortal. \n
+    :param molecular_profile_ids: List of Molecular Profile IDs (e.g., ["brca_tcga_gistic", "brca_tcga_mutations", "acc_tcga_gistic"]). \n
+    :type molecular_profile_ids: list of str \n
+    :param molecular_prof_sample_ids: List of Molecular Profile ID and Sample IDs pairs. \n
+    :type molecular_prof_sample_ids: list of dict \n
+        Each dict should have the following format: \n
             molecular_prof_sample_ids = [
                                         {"molecular_profile_id": "brca_tcga_gistic",  
                                          "sample_ids": ["TCGA-AR-A1AR-01", "TCGA-E2-A1BC-01"]},
@@ -22,8 +21,8 @@ def fetch_gene_panel_data(molecular_profile_ids=None, molecular_prof_sample_ids=
                                         {"molecular_profile_id": "msk_met_2021_mutations", 
                                          "sample_ids": ["P-0000004-T01-IM3", "P-0000950-T01-IM3"]}
                                         ]
-    :returns: A DataFrame containing the fetched gene panel data.
-    :rtype: pandas.DataFrame
+    :returns: A DataFrame containing the fetched gene panel data. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/gene-panel-data/fetch"
 
@@ -47,19 +46,19 @@ def fetch_gene_panel_data(molecular_profile_ids=None, molecular_prof_sample_ids=
                 gene_panel_data_filter["sampleMolecularIdentifiers"].append(identifier)
 
     response = requests.post(f"{base_url}{endpoint}", json=gene_panel_data_filter)
-    return check_response(response, "Failed to fetch gene panel data.")
+    return process_response(response, "Failed to fetch gene panel data.")
 
-def get_gene_panel_data(molecular_profile_id, sample_ids=None, sample_list_id=None):
+def fetch_gene_panel_data_in_molecular_profile(molecular_profile_id, sample_ids=None, sample_list_id=None):
     """
-    Get gene panel data for a specific molecular profile.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "brca_tcga_mutations".
-    :type molecular_profile_id: str
-    :param sample_ids: List of Sample IDs, e.g., ["TCGA-AR-A1AR-01", "TCGA-E2-A1BC-01"] and sample_list_id set to None.
-    :type sample_ids: list of str
-    :param sample_list_id: Sample List ID, e.g., "brca_tcga_all" and sample_ids set to None.
-    :type sample_list_id: str
-    :returns: A DataFrame containing the fetched gene panel data.
-    :rtype: pandas.DataFrame
+    Get gene panel data for a specific molecular profile. \n
+    :param molecular_profile_id: Molecular Profile ID (e.g., "brca_tcga_mutations"). \n
+    :type molecular_profile_id: str \n
+    :param sample_ids: List of Sample IDs (e.g., ["TCGA-AR-A1AR-01", "TCGA-E2-A1BC-01"] and sample_list_id set to None). \n
+    :type sample_ids: list of str \n
+    :param sample_list_id: Sample List ID (e.g., "brca_tcga_all" and sample_ids set to None). \n
+    :type sample_list_id: str \n
+    :returns: A DataFrame containing the fetched gene panel data. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/molecular-profiles/{molecular_profile_id}/gene-panel-data/fetch"
 
@@ -69,4 +68,4 @@ def get_gene_panel_data(molecular_profile_id, sample_ids=None, sample_list_id=No
            }
 
     response = requests.post(f"{base_url}{endpoint}", json=gene_panel_data_filter)
-    return check_response(response, "Failed to get gene panel data.")
+    return process_response(response, "Failed to fetch gene panel data.")
