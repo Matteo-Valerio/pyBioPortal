a4cd374dcfe2f930a1e94ba9ca3c0faa334be6f8
Matteo Valerio <matteo.valerio@live.it>
2023-11-07 21:30:58 +0100

    format doc string

--- pyBioGate/info.py
+++ pyBioGate/info.py
@@ -1,17 +1,16 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ########
 # Info #
 ########
 def get_info():
     """
-    Get information about the running instance.
-    :returns: A DataFrame containing information about the running instance.
-    :rtype: pandas.DataFrame
+    Get information about the running instance. \n
+    :returns: A DataFrame containing information about the running instance. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/info"
     response = requests.get(f"{base_url}{endpoint}")
-    return check_response(response, "Failed to get information.")
+    return process_response(response, "Failed to get information.")
--- pyBioGate/molecular_data.py
+++ pyBioGate/molecular_data.py
@@ -1,35 +1,35 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ##################
 # Molecular Data #
 ##################
 def fetch_molecular_data(entrez_gene_ids=None, molecular_profile_ids=None, sample_molecular_identifiers=None, projection="SUMMARY"):
     """
-    Fetch molecular data.
-    :param entrez_gene_ids: List of Entrez Gene IDs, e.g., ["672", "675"]
-    :type entrez_gene_ids: list of str
-    :param molecular_profile_ids: List of MolecularProfile IDs, e.g., ["brca_tcga_mrna", "acc_tcga_rna_seq_v2_mrna"]
-    :type molecular_profile_ids: list of str
-    :param sample_molecular_identifiers: List of Molecular Profile ID and Sample ID pairs.
-    :type sample_molecular_identifiers: list of dict
-        Each dict should have the following format:
+    Fetch molecular data. \n
+    :param entrez_gene_ids: List of Entrez Gene IDs (e.g., ["672", "675"]). \n
+    :type entrez_gene_ids: list of str \n
+    :param molecular_profile_ids: List of MolecularProfile IDs (e.g., ["brca_tcga_mrna", "acc_tcga_rna_seq_v2_mrna"]). \n
+    :type molecular_profile_ids: list of str \n
+    :param sample_molecular_identifiers: List of Molecular Profile ID and Sample ID pairs. \n
+    :type sample_molecular_identifiers: list of dict \n
+        Each dict should have the following format: \n
             sample_molecular_identifiers = [
                                            {"molecular_profile_id": "brca_tcga_mrna", 
                                             "sample_ids": ["TCGA-AR-A1AR-01","TCGA-BH-A1EO-01"]},
                                            {"molecular_profile_id": "acc_tcga_rna_seq_v2_mrna", 
                                             "sample_ids": ["TCGA-OR-A5J1-01","TCGA-OR-A5J2"]}
                                            ]
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str
-    :returns: A DataFrame containing molecular data.
-    :rtype: pandas.DataFrame
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing molecular data. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/molecular-data/fetch"
     params = {"projection": projection}
@@ -56,26 +56,27 @@ def fetch_molecular_data(entrez_gene_ids=None, molecular_profile_ids=None, sampl
                 }
                 molecular_data_filter["sampleMolecularIdentifiers"].append(identifier)
 
-    response = requests.post(f"{base_url}{endpoint}", json=molecular_data_filter, params=params)
-    return check_response(response, "Failed to fetch molecular data.")
+    response = requests.post(f"{base_url}{endpoint}", params=params, json=molecular_data_filter)
+    return process_response(response, "Failed to fetch molecular data.")
         
 def get_all_molecular_data_in_molecular_profile(molecular_profile_id, sample_list_id, entrez_gene_id, projection="SUMMARY"):
     """
-    Get all molecular data in a molecular profile for a specific gene.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "acc_tcga_rna_seq_v2_mrna".
-    :type molecular_profile_id: str
-    :param sample_list_id: Sample List ID, e.g., "acc_tcga_all".
-    :type sample_list_id: str
-    :param entrez_gene_id: Entrez Gene ID, e.g., 1.
-    :type entrez_gene_id: int
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :returns: A DataFrame containing molecular data for the specified gene.
-    :rtype: pandas.DataFrame
+    Get all molecular data in a molecular profile for a specific gene. \n
+    :param molecular_profile_id: Molecular Profile ID (e.g., "acc_tcga_rna_seq_v2_mrna"). \n
+    :type molecular_profile_id: str \n
+    :param sample_list_id: Sample List ID (e.g., "acc_tcga_all"). \n
+    :type sample_list_id: str \n
+    :param entrez_gene_id: Entrez Gene ID (e.g., "1"). \n
+    :type entrez_gene_id: str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing molecular data for the specified gene. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/molecular-profiles/{molecular_profile_id}/molecular-data"
     params = {
@@ -85,23 +86,29 @@ def get_all_molecular_data_in_molecular_profile(molecular_profile_id, sample_lis
     }
 
     response = requests.get(f"{base_url}{endpoint}", params=params)
-    return check_response(response, "Failed to get molecular data in molecular profile.")
+    return process_response(response, "Failed to get molecular data in molecular profile.")
 
-def fetch_all_molecular_data_in_molecular_profile(molecular_profile_id, entrez_gene_ids = None, sample_ids = None, sample_list_id = None, projection="SUMMARY"):
+def fetch_all_molecular_data_in_molecular_profile(molecular_profile_id, entrez_gene_ids = None, sample_ids = None, 
+                                                  sample_list_id = None, projection="SUMMARY"):
     """
-    Fetch molecular data in a molecular profile for a list of genes.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "acc_tcga_rna_seq_v2_mrna".
-    :type molecular_profile_id: str
-    :param molecular_data_filter: List of Sample IDs/Sample List ID and Entrez Gene IDs.
-    :type molecular_data_filter: dict
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :returns: A DataFrame containing molecular data for the specified genes.
-    :rtype: pandas.DataFrame
+    Fetch molecular data in a molecular profile for a list of genes. \n
+    :param molecular_profile_id: Molecular Profile ID (e.g., "acc_tcga_rna_seq_v2_mrna"). \n
+    :type molecular_profile_id: str \n
+    :param entrez_gene_ids: List of Entrez Gene IDs (e.g., ["672","675"]). \n
+    :type entrez_gene_ids: list of str \n
+    :param sample_ids: List of Sample IDs (e.g., ["TCGA-AR-A1AR-01","TCGA-BH-A1EO-01"]). \n
+    :type sample_ids: list of str \n
+    :param sample_list_id: Sample List ID (e.g., "brca_tcga_all"). \n
+    :type sample_list_id: str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing molecular data for the specified genes. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/molecular-profiles/{molecular_profile_id}/molecular-data/fetch"
     params = {
@@ -119,5 +126,5 @@ def fetch_all_molecular_data_in_molecular_profile(molecular_profile_id, entrez_g
     if sample_list_id:
         molecular_data_filter['sampleListId'] = sample_list_id
 
-    response = requests.post(f"{base_url}{endpoint}", json=molecular_data_filter, params=params)
-    return check_response(response, "Failed to fetch molecular data in molecular profile.")
+    response = requests.post(f"{base_url}{endpoint}", params=params, json=molecular_data_filter)
+    return process_response(response, "Failed to fetch molecular data in molecular profile.")
