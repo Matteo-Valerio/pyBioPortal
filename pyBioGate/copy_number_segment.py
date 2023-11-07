af0823bb0af07d09bc1003ffd8e927d39caff6f5
Matteo Valerio <matteo.valerio@live.it>
2023-11-07 12:29:26 +0100

    reformat docstring

--- pyBioGate/copy_number_segment.py
+++ pyBioGate/copy_number_segment.py
@@ -1,46 +1,39 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ########################
 # Copy Number Segments #
 ########################
 def fetch_copy_number_segments(sample_study_ids, chromosome=None, projection="SUMMARY"):
     """
-    Fetch copy number segments from BioPortal by sample ID.
-    :param sample_study_ids: List of sample identifiers.
-    :type sample_study_ids: list of dict
-        Each dict should have the following format:
-        e.g. for PATIENT data type:
-            sample_study_ids = [
-                               {"sample_ids": ["P-0000004", "P-0000950"], 
-                                "study_id": "msk_met_2021"},
-                               {"sample_ids": ["TCGA-5T-A9QA", "TCGA-A1-A0SB"], 
-                                "study_id": "brca_tcga"}
-                               ]
-        e.g. for SAMPLE data type:
+    Fetch copy number segments from BioPortal by sample ID. \n
+    :param sample_study_ids: List of sample identifiers. \n
+    :type sample_study_ids: list of dict \n        
+        Each list should have the following format: \n
             sample_study_ids = [
                                {"sample_ids": ["P-0000004-T01-IM3", "P-0000950-T01-IM3"], 
                                 "study_id": "msk_met_2021"},
                                {"sample_ids": ["TCGA-5T-A9QA-01", "TCGA-A1-A0SB-01"], 
                                 "study_id": "brca_tcga"}
                                ]
-    :param chromosome: Chromosome (optional).
-    :type chromosome: str
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str
-    :returns: A DataFrame containing the fetched copy number segments.
-    :rtype: pandas.DataFrame
+    :param chromosome: Chromosome (e.g., "1"). \n
+    :type chromosome: str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing the fetched copy number segments. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/copy-number-segments/fetch"
-    params = {"projection": projection}
-    if chromosome:
-        params["chromosome"] = chromosome
+    params = {"projection": projection,
+              "chromosome": chromosome}
+    # if chromosome:
+    #     params["chromosome"] = chromosome
 
     sample_identifiers = []
     
@@ -55,41 +48,47 @@ def fetch_copy_number_segments(sample_study_ids, chromosome=None, projection="SU
             }
             sample_identifiers.append(identifier)
 
-    response = requests.post(f"{base_url}{endpoint}", json=sample_identifiers, params=params)
-    return check_response(response, "Failed to fetch copy number segments.")
+    response = requests.post(f"{base_url}{endpoint}", params=params, json=sample_identifiers,)
+    return process_response(response, "Failed to fetch copy number segments.")
 
-def get_copy_number_segments_in_sample_in_study(study_id, sample_id, chromosome=None, direction="ASC", pageNumber=0, pageSize=20000, projection="SUMMARY", sortBy="chromosome"):
+def get_copy_number_segments_in_sample_in_study(study_id, sample_id, chromosome=None, direction="ASC", 
+                                                pageNumber=0, pageSize=20000, projection="SUMMARY", sortBy="chromosome"):
     """
-    Get copy number segments in a sample in a study.
-    :param study_id: Study ID, e.g., "acc_tcga".
-    :type study_id: str
-    :param sample_id: Sample ID, e.g., "TCGA-OR-A5J2-01".
-    :type sample_id: str
-    :param chromosome: Chromosome, e.g., "1".
-    :type chromosome: str, optional
-    :param direction: Direction of the sort.
-        - "ASC": Ascending.
-        - "DESC": Descending.
-    :type direction: str, optional, default: "ASC"
-    :param pageNumber: Page number of the result list.
-    :type pageNumber: int, optional, default: 0
-    :param pageSize: Page size of the result list.
-    :type pageSize: int, optional, default: 20000
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :param sortBy: Name of the property that the result list is sorted by.
-        - "chromosome": Sort by chromosome.
-        - "end": Sort by end position.
-        - "numberOfProbes": Sort by the number of probes.
-        - "segmentMean": Sort by segment mean.
-        - "start": Sort by start position.
-    :type sortBy: str, optional, default: "chromosome"
-    :returns: List of copy number segments for the specified sample in the study.
-    :rtype: list[dict]
+    Get copy number segments in a sample in a study. \n
+    :param study_id: Study ID (e.g., "acc_tcga"). \n
+    :type study_id: str \n
+    :param sample_id: Sample ID (e.g., "TCGA-OR-A5J2-01"). \n
+    :type sample_id: str \n
+    :param chromosome: Chromosome (e.g., "1"). \n
+    :type chromosome: str \n
+    :param direction: Direction of the sort. \n
+        Possible values: \n
+            - "ASC": Ascending (default).
+            - "DESC": Descending.
+    :type direction: str \n
+    :param pageNumber: Page number of the result list. \n
+            - Minimum value is 0.
+    :type pageNumber: int \n
+    :param pageSize: Page size of the result list. \n
+            - Minimum value is 1, maximum value is 20000.
+    :type pageSize: int \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :param sortBy: Name of the property that the result list is sorted by. \n
+        Possible values: \n
+            - "chromosome": Sort by chromosome (default).
+            - "end": Sort by end position.
+            - "numberOfProbes": Sort by the number of probes.
+            - "segmentMean": Sort by segment mean.
+            - "start": Sort by start position.
+    :type sortBy: str \n
+    :returns: A DataFrame containing copy number segments for the specified sample in the study. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/studies/{study_id}/samples/{sample_id}/copy-number-segments"
     params = {
@@ -97,11 +96,13 @@ def get_copy_number_segments_in_sample_in_study(study_id, sample_id, chromosome=
         "pageNumber": pageNumber,
         "pageSize": pageSize,
         "projection": projection,
+        "chromosome": chromosome,
+        "sortBy": sortBy 
     }
-    if chromosome:
-        params["chromosome"] = chromosome
-    if sortBy:
-        params["sortBy"] = sortBy
+    # if chromosome:
+    #     params["chromosome"] = chromosome
+    # if sortBy:
+    #     params["sortBy"] = sortBy
 
     response = requests.get(f"{base_url}{endpoint}", params=params)
-    return check_response(response, "Failed to get copy number segments for the sample.")
+    return process_response(response, "Failed to get copy number segments for the sample.")
--- pyBioGate/discrete_copy_number_alterations.py
+++ pyBioGate/discrete_copy_number_alterations.py
@@ -1,80 +1,84 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ####################################
 # Discrete Copy Number Alterations #
 ####################################
 
-def get_discrete_copy_numbers_in_molecular_profile(molecular_profile_id, sample_list_id, discrete_copy_number_event_type="HOMDEL_AND_AMP", projection="SUMMARY"):
+def get_discrete_copy_numbers_in_molecular_profile(molecular_profile_id, sample_list_id, discrete_cn_evt_type="HOMDEL_AND_AMP", projection="SUMMARY"):
     """
-    Get discrete copy number alterations in a molecular profile by sample list ID.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "acc_tcga_gistic".
-    :type molecular_profile_id: str
-    :param sample_list_id: Sample List ID, e.g., "acc_tcga_all".
-    :type sample_list_id: str
-    :param discrete_copy_number_event_type: Type of the copy number event.
-        - "ALL": All events.
-        - "AMP": Amplification.
-        - "DIPLOID": Diploid.
-        - "GAIN": Gain.
-        - "HETLOSS": Heterozygous loss.
-        - "HOMDEL": Homozygous deletion.
-        - "HOMDEL_AND_AMP": Homozygous deletion and amplification (default).
-    :type discrete_copy_number_event_type: str, optional, default: "HOMDEL_AND_AMP"
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :returns: List of discrete copy number alterations.
-    :rtype: list[dict]
+    Get discrete copy number alterations in a molecular profile by sample list ID. \n
+    :param molecular_profile_id: Molecular Profile ID (e.g., "acc_tcga_gistic"). \n
+    :type molecular_profile_id: str \n
+    :param sample_list_id: Sample List ID (e.g., "acc_tcga_all"). \n
+    :type sample_list_id: str \n
+    :param discrete_cn_evt_type: Type of the copy number event. \n
+        Possible values: \n
+            - "ALL": All events.
+            - "AMP": Amplification.
+            - "DIPLOID": Diploid.
+            - "GAIN": Gain.
+            - "HETLOSS": Heterozygous loss.
+            - "HOMDEL": Homozygous deletion.
+            - "HOMDEL_AND_AMP": Homozygous deletion and amplification (default).
+    :type discrete_cn_evt_type: str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing discrete copy number alterations. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/molecular-profiles/{molecular_profile_id}/discrete-copy-number"
     params = {
-        "discreteCopyNumberEventType": discrete_copy_number_event_type,
+        "discreteCopyNumberEventType": discrete_cn_evt_type,
         "projection": projection,
         "sampleListId": sample_list_id
     }
 
     response = requests.get(f"{base_url}{endpoint}", params=params)
-    return check_response(response, "Failed to get discrete copy numbers in molecular profile.")
+    return process_response(response, "Failed to get discrete copy numbers in molecular profile.")
 
 
-def fetch_discrete_copy_numbers_in_molecular_profile(molecular_profile_id, entrez_gene_ids=None, sample_ids=None, sample_list_id=None, discrete_copy_number_event_type="HOMDEL_AND_AMP", projection="SUMMARY"):
+def fetch_discrete_copy_numbers_in_molecular_profile(molecular_profile_id, entrez_gene_ids=None, sample_ids=None, sample_list_id=None, 
+                                                     discrete_cn_evt_type="HOMDEL_AND_AMP", projection="SUMMARY"):
     """
-    Fetch discrete copy number alterations in a molecular profile by sample list ID.
-    :param molecular_profile_id: Molecular Profile ID, e.g., "brca_tcga_gistic".
-    :type molecular_profile_id: str
-    :param entrez_gene_ids: List of Entrez Gene IDs, e.g., ["2023", "4853", "54940"].
-    :type entrez_gene_ids: list of str
-    :param sample_ids: List of Sample IDs, e.g., ["TCGA-AR-A1AR-01", "TCGA-E2-A1BC-01"] and sample_list_id set to None.
-    :type sample_ids: list of str
-    :param sample_list_id: Sample List ID, e.g., "acc_tcga_all" and sample_ids set to None.
-    :type sample_list_id: str
-    :param discrete_copy_number_event_type: Type of the copy number event.
-        - "ALL": All events.
-        - "AMP": Amplification.
-        - "DIPLOID": Diploid.
-        - "GAIN": Gain.
-        - "HETLOSS": Heterozygous loss.
-        - "HOMDEL": Homozygous deletion.
-        - "HOMDEL_AND_AMP": Homozygous deletion and amplification (default).
-    :type discrete_copy_number_event_type: str, optional, default: "HOMDEL_AND_AMP"
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :returns: List of discrete copy number alterations.
-    :rtype: list[dict]
+    Fetch discrete copy number alterations in a molecular profile by sample list ID. \n
+    :param molecular_profile_id: Molecular Profile ID (e.g., "brca_tcga_gistic"). \n
+    :type molecular_profile_id: str \n
+    :param entrez_gene_ids: List of Entrez Gene IDs (e.g., ["2023", "4853", "54940"]). \n
+    :type entrez_gene_ids: list of str \n
+    :param sample_ids: List of Sample IDs (e.g., ["TCGA-AR-A1AR-01", "TCGA-E2-A1BC-01"] and sample_list_id set to None). \n
+    :type sample_ids: list of str \n
+    :param sample_list_id: Sample List ID (e.g., "acc_tcga_all" and sample_ids set to None). \n
+    :type sample_list_id: str \n
+    :param discrete_cn_evt_type: Type of the copy number event. \n
+        Possible values: \n
+            - "ALL": All events.
+            - "AMP": Amplification.
+            - "DIPLOID": Diploid.
+            - "GAIN": Gain.
+            - "HETLOSS": Heterozygous loss.
+            - "HOMDEL": Homozygous deletion.
+            - "HOMDEL_AND_AMP": Homozygous deletion and amplification (default).
+    :type discrete_cn_evt_type: str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing discrete copy number alterations. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/molecular-profiles/{molecular_profile_id}/discrete-copy-number/fetch"
     params = {
-        "discreteCopyNumberEventType": discrete_copy_number_event_type,
+        "discreteCopyNumberEventType": discrete_cn_evt_type,
         "projection": projection
     }
     
@@ -85,4 +89,4 @@ def fetch_discrete_copy_numbers_in_molecular_profile(molecular_profile_id, entre
            }
 
     response = requests.post(f"{base_url}{endpoint}", params=params, json=discreteCopyNumberFilter)
-    return check_response(response, "Failed to fetch discrete copy numbers in molecular profile.")
+    return process_response(response, "Failed to fetch discrete copy numbers in molecular profile.")
