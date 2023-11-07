83b29944c760647adad7d696fddbefb1b54555ed
Matteo Valerio <matteo.valerio@live.it>
2023-11-07 11:39:52 +0100

    commented

--- pyBioGate/config.py
+++ pyBioGate/config.py
@@ -1,7 +1,7 @@
-# Variabile comune per l'URL di base
+# Global variable
 base_url = "https://www.cbioportal.org/api"
 
-# Funzione di configurazione per l'URL di base
+# Setting base url
 def configure_base_url(new_base_url=base_url):
     global base_url
     base_url = new_base_url
