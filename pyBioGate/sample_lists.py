3176f7eca3f7c5bd0de19fd328b568fb03fc2501
Matteo Valerio <matteo.valerio@live.it>
2023-11-08 09:34:42 +0100

    sample lists corrected doc string

--- pyBioGate/sample_lists.py
+++ pyBioGate/sample_lists.py
@@ -1,38 +1,41 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 ################
 # Sample Lists #
 ################
-def get_all_sample_lists(projection="SUMMARY", direction="ASC", pageNumber=0, pageSize=10000000, sortBy="sampleListId"):
+def get_all_sample_lists(projection="SUMMARY", direction="ASC", pageNumber=0, pageSize=10000000, sortBy=None):
     """
-    Get all sample lists.
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :param direction: Direction of the sort.
-        - "ASC": Ascending.
-        - "DESC": Descending.
-    :type direction: str, optional, default: "ASC"
-    :param pageNumber: Page number of the result list.
-    :type pageNumber: int, optional, default: 0
-    :param pageSize: Page size of the result list.
-    :type pageSize: int, optional, default: 10000000
-    :param sortBy: Name of the property that the result list is sorted by.
-        Possible values:
-        - "category"
-        - "description"
-        - "name"
-        - "sampleListId"
-        - "studyId"
-    :type sortBy: str, optional, default: "sampleListId"
-    :returns: A DataFrame containing sample lists.
-    :rtype: pandas.DataFrame
+    Get all sample lists. \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :param direction: Direction of the sort. \n
+        Possible values: \n
+            - "ASC": Ascending (default).
+            - "DESC": Descending.
+    :type direction: str \n
+    :param pageNumber: Page number of the result list. \n
+            - Minimum value is 0.
+    :type pageNumber: int \n
+    :param pageSize: Page size of the result list. \n
+            - Minimum value is 1, maximum value is 10000000.
+    :type pageSize: int \n
+    :param sortBy: Name of the property that the result list is sorted by. \n
+        Possible values: \n
+            - "category"
+            - "description"
+            - "name"
+            - "sampleListId"
+            - "studyId"
+    :type sortBy: str \n
+    :returns: A DataFrame containing sample lists. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/sample-lists"
     params = {
@@ -44,83 +47,90 @@ def get_all_sample_lists(projection="SUMMARY", direction="ASC", pageNumber=0, pa
     }
 
     response = requests.get(f"{base_url}{endpoint}", params=params)
-    return check_response(response, "Failed to get all sample lists.")
+    return process_response(response, "Failed to get all sample lists.")
 
 def get_sample_list(sample_list_id):
     """
-    Get sample list.
-    :param sample_list_id: Sample List ID, e.g., "acc_tcga_all".
-    :type sample_list_id: str
-    :returns: A DataFrame containing sample list details.
-    :rtype: pandas.DataFrame
+    Get sample list. \n
+    :param sample_list_id: Sample List ID (e.g., "acc_tcga_all"). \n
+    :type sample_list_id: str \n
+    :returns: A DataFrame containing sample list details. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/sample-lists/{sample_list_id}"
 
     response = requests.get(f"{base_url}{endpoint}")
-    return check_response(response, "Failed to get sample list.")
+    return process_response(response, "Failed to get sample list.")
 
 def get_all_sample_ids_in_sample_list(sample_list_id):
     """
-    Get all sample IDs in a sample list.
-    :param sample_list_id: Sample List ID, e.g., "acc_tcga_all".
-    :type sample_list_id: str
-    :returns: A DataFrame containing sample IDs in the sample list.
-    :rtype: pandas.DataFrame
+    Get all sample IDs in a sample list. \n
+    :param sample_list_id: Sample List ID (e.g., "acc_tcga_all"). \n
+    :type sample_list_id: str \n
+    :returns: A DataFrame containing sample IDs in the sample list. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/sample-lists/{sample_list_id}/sample-ids"
+
     response = requests.get(f"{base_url}{endpoint}")
-    return check_response(response, "Failed to get all sample IDs in sample list.")
+    return process_response(response, "Failed to get all sample IDs in sample list.")
     
 def fetch_sample_lists(sample_list_ids, projection="SUMMARY"):
     """
-    Fetch sample lists by ID.
-    :param sample_list_ids: List of sample list IDs.
-    :type sample_list_ids: list[str]
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :returns: A DataFrame containing sample lists.
-    :rtype: pandas.DataFrame
+    Fetch sample lists by ID. \n
+    :param sample_list_ids: List of sample list IDs. \n
+    :type sample_list_ids: list of str \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :returns: A DataFrame containing sample lists. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/sample-lists/fetch"
     params = {
         "projection": projection
     }
 
-    response = requests.post(f"{base_url}{endpoint}", json=sample_list_ids, params=params)
-    return check_response(response, "Failed to fetch sample lists.")
+    response = requests.post(f"{base_url}{endpoint}", params=params, json=sample_list_ids)
+    return process_response(response, "Failed to fetch sample lists.")
         
-def get_all_sample_lists_in_study(study_id, direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy="sampleListId"):
+def get_all_sample_lists_in_study(study_id, direction="ASC", pageNumber=0, pageSize=10000000, projection="SUMMARY", sortBy=None):
     """
-    Get all sample lists in a study.
-    :param study_id: Study ID, e.g., "acc_tcga".
-    :type study_id: str
-    :param direction: Direction of the sort.
-        - "ASC": Ascending.
-        - "DESC": Descending.
-    :type direction: str, optional, default: "ASC"
-    :param pageNumber: Page number of the result list.
-    :type pageNumber: int, optional, default: 0
-    :param pageSize: Page size of the result list.
-    :type pageSize: int, optional, default: 10000000
-    :param projection: Level of detail of the response.
-        - "DETAILED": Detailed information.
-        - "ID": Information with only IDs.
-        - "META": Metadata information.
-        - "SUMMARY": Summary information (default).
-    :type projection: str, optional, default: "SUMMARY"
-    :param sortBy: Name of the property that the result list is sorted by.
-        - "category": Sort by category.
-        - "description": Sort by description.
-        - "name": Sort by name.
-        - "sampleListId": Sort by sample list ID.
-        - "studyId": Sort by study ID.
-    :type sortBy: str, optional, default: "sampleListId"
-    :returns: A DataFrame containing sample lists in the specified study.
-    :rtype: pandas.DataFrame
+    Get all sample lists in a study. \n
+    :param study_id: Study ID (e.g., "acc_tcga"). \n
+    :type study_id: str \n
+    :param direction: Direction of the sort. \n
+        Possible values: \n
+            - "ASC": Ascending (default).
+            - "DESC": Descending.
+    :type direction: str \n
+    :param pageNumber: Page number of the result list. \n
+            - Minimum value is 0.
+    :type pageNumber: int \n
+    :param pageSize: Page size of the result list. \n
+            - Minimum value is 1, maximum value is 10000000.
+    :type pageSize: int \n
+    :param projection: Level of detail of the response. \n
+        Possible values: \n
+            - "DETAILED": Detailed information.
+            - "ID": Information with only IDs.
+            - "META": Metadata information.
+            - "SUMMARY": Summary information (default).
+    :type projection: str \n
+    :param sortBy: Name of the property that the result list is sorted by. \n
+        Possible values: \n
+            - "category": Sort by category.
+            - "description": Sort by description.
+            - "name": Sort by name.
+            - "sampleListId": Sort by sample list ID.
+            - "studyId": Sort by study ID.
+    :type sortBy: str \n
+    :returns: A DataFrame containing sample lists in the specified study. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = f"/studies/{study_id}/sample-lists"
     params = {
@@ -132,4 +142,4 @@ def get_all_sample_lists_in_study(study_id, direction="ASC", pageNumber=0, pageS
     }
     
     response = requests.get(f"{base_url}{endpoint}", params=params)
-    return check_response(response, "Failed to get sample lists in the study.")
+    return process_response(response, "Failed to get sample lists in the study.")
