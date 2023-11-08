dfd5864f3b38d98ab5ce741c3f89b78c975ba780
Matteo Valerio <matteo.valerio@live.it>
2023-11-08 09:51:32 +0100

    server stat corrected doc string

--- pyBioGate/server_running_status.py
+++ pyBioGate/server_running_status.py
@@ -1,17 +1,16 @@
 import requests
-import pandas as pd
-from config import base_url
-from aux_funcs import check_response
+from .config import base_url
+from .aux_funcs import process_response
 
 #########################
 # Server running status #
 #########################
 def get_server_status():
     """
-    Get the running status of the server.
-    :returns: A DataFrame containing the status of the server.
-    :rtype: pandas.DataFrame
+    Get the running status of the server. \n
+    :returns: A DataFrame containing the status of the server. \n
+    :rtype: pandas.DataFrame \n
     """
     endpoint = "/health"
     response = requests.get(f"{base_url}{endpoint}")
-    return check_response(response, "Failed to get server status.")
+    return process_response(response, "Failed to get server status.")
