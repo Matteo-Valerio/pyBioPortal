63c56e86b48d9ae0dd07e582bb9a5c696549637d
Matteo Valerio <matteo.valerio@live.it>
2023-11-27 18:54:19 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 
 project = 'pybioportal'
-copyright = '2023, `Matteo Valerio <https://pybioportal.readthedocs.io/en/latest/>`__'
+copyright = '2023, Matteo Valerio'
 author = 'Matteo Valerio'
 version = '1.0.0'
 
