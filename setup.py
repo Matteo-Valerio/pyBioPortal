c16f8e14fbd00c33ce0a4ba0dd75bf9719d99c16
Matteo Valerio <matteo.valerio@live.it>
2023-12-05 22:11:12 +0100

    conda and pypi setup

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+if __name__ == "__main__":
+    import sys
+
+    script_args = sys.argv[1:]
+    if 'sdist' in script_args:
+        # output folder for the distributable package
+        script_args += ['--dist-dir', 'nome_cartella_output']
+
 with open('LICENSE.txt', 'r') as file:
     license_text = file.read()
 
@@ -8,6 +16,7 @@ version = "1.0.0"
 description = "A Python package to easily retrieve data from cBioPortal."
 author = "Matteo Valerio"
 url = "https://github.com/Matteo-Valerio/pyBioPortal"
+doc_url = "https://pybioportal.readthedocs.io"
 license = license_text
 keywords = "cBioPortal, API, bioinformatics"
 
@@ -26,6 +35,9 @@ setup(
     author=author,
     #author_email=author_email,
     url=url,
+    project_urls={
+        "Documentation": doc_url
+    },
     license=license,
     keywords=keywords,
     install_requires=install_requires,
