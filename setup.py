e48f2f66d8636d64b3aec56b125a948d50f6209d
Matteo Valerio <matteo.valerio@live.it>
2023-12-05 14:41:04 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,46 +1,20 @@
 from setuptools import setup, find_packages
 
+with open('LICENSE.txt', 'r') as file:
+    license_text = file.read()
+
 name = "pybioportal"
 version = "1.0.0"
 description = "A Python package to easily retrieve data from cBioPortal."
 author = "Matteo Valerio"
 url = "https://github.com/Matteo-Valerio/pyBioPortal"
-license = """
-BSD 3-Clause License
-
-Copyright (c) 2023 Matteo Valerio
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, 
-   this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, 
-   this list of conditions and the following disclaimer in the documentation 
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors 
-   may be used to endorse or promote products derived from this software without 
-   specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
-OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
+license = license_text
 keywords = "cBioPortal, API, bioinformatics"
 
 install_requires = [
     "requests>=2.0.0",
-    "pandas>=1.0.0"
+    "pandas>=1.0.0",
+    "numpy>=1.23.0"
 ]
 
 packages = find_packages()
