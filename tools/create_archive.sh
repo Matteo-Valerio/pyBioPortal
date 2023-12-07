9cdc108bc0d94c352cc6bf5d89e1febc13dbae02
Matteo Valerio <matteo.valerio@live.it>
2023-12-07 22:25:01 +0100

    Create new archive v1.0.0

--- tools/create_archive.sh
+++ tools/create_archive.sh
@@ -7,7 +7,7 @@ VERSION="v1.0.0"
 cd ..
 
 git pull origin master
-git archive --format=tar.gz -o ./archive/pybioportal-"$VERSION".tar.gz HEAD:pybioportal
+git archive --format=tar.gz -o ./archive/pybioportal-"$VERSION".tar.gz pybioportal
 git add .
 git commit -m "Create new archive $VERSION"
 git push origin master
