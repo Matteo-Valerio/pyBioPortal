2b5e236b12e5afb103d15861ed1dc1845aa677f4
Matteo Valerio <matteo.valerio@live.it>
2023-12-08 15:21:22 +0100

    import conf_build

---
+++ tools/run_conda_build.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+from conf_build import vVERSION
+
+output_folder = f"dist\\{vVERSION}\\anaconda"
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# commands to execute
+commands = [
+    f'conda build . --output-folder {output_folder}',
+    'conda build purge'
+]
+
+print(f'Current Directory: {os.getcwd()}',)
+
+for cmd in commands:
+    print(f'>>{cmd}: \n')
+    subprocess.run(cmd, shell=True)
