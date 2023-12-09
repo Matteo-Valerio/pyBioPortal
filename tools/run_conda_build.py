77b0bfbb2023f29afd7ab0c911af14eca31962b5
Matteo Valerio <matteo.valerio@live.it>
2023-12-09 11:18:35 +0100

    Create new archive 1.0.0

--- tools/run_conda_build.py
+++ tools/run_conda_build.py
@@ -19,3 +19,13 @@ print(f'Current Directory: {os.getcwd()}',)
 for cmd in commands:
     print(f'>>{cmd}: \n')
     subprocess.run(cmd, shell=True)
+
+# command git add .
+subprocess.run(['git', 'add', '.'])
+
+# command git commit
+commit_message = f"Build version {vVERSION}"
+subprocess.run(['git', 'commit', '-m', commit_message])
+
+# command git push origin master
+subprocess.run(['git', 'push', 'origin', 'master'])
