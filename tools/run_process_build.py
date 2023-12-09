1db2ddebae8440f4d62a32d7bbfad798a0f1627d
Matteo Valerio <matteo.valerio@live.it>
2023-12-09 10:00:42 +0100

    Create new archive 1.0.0

---
+++ tools/run_process_build.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# script list to run
+scripts_to_run = ['update_meta_yaml.py', 'create_archive.py', 'run_conda_build.py']
+
+for script in scripts_to_run:
+    subprocess.run(['python', script])
