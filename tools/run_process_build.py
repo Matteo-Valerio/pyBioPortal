23619c0131523c1dadec1264532f06d961f05e4c
Matteo Valerio <matteo.valerio@live.it>
2023-12-09 15:30:14 +0100

    list script process

--- tools/run_process_build.py
+++ tools/run_process_build.py
@@ -1,7 +1,10 @@
 import subprocess
 
 # script list to run
-scripts_to_run = ["update_meta_yaml.py", "create_archive.py", "run_pypi_build.py", "run_conda_build.py"]
+scripts_to_run = ["update_meta_yaml.py", 
+                  "create_archive.py", 
+                  "run_pypi_build.py", 
+                  "run_conda_build.py"]
 
 for script in scripts_to_run:
     subprocess.run(["python", script])
