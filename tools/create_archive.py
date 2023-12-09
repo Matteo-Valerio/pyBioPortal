0e141340739c01b8ea753a35eed59118f66fbe4c
Matteo Valerio <matteo.valerio@live.it>
2023-12-09 09:17:10 +0100

    clean create_archive file

--- tools/create_archive.py
+++ tools/create_archive.py
@@ -2,23 +2,22 @@ import tarfile
 import os
 from conf_build import vVERSION
 
-def crea_tar_gz(nome_file_output, cartelle, file_da_aggiungere):
-    with tarfile.open(nome_file_output, "w:gz") as tar:
-        # Aggiungi cartelle al file tar.gz
-        for cartella in cartelle:
-            tar.add(cartella, arcname=os.path.basename(cartella))
+def create_tar_gz(output_file, folders, files):
+    with tarfile.open(output_file, "w:gz") as tar:
+        # Add folders to file tar.gz
+        for folder in folders:
+            tar.add(folder, arcname=os.path.basename(folder))
 
-        # Aggiungi file al file tar.gz
-        for file in file_da_aggiungere:
+        # Add files to file tar.gz
+        for file in files:
             tar.add(file, arcname=os.path.basename(file))
 
 if __name__ == "__main__":
-    # Definisci le cartelle e i file da includere nel file tar.gz
-    cartelle_da_aggiungere = ["../pybioportal"]
-    file_da_aggiungere = ["../setup.py", "../LICENSE.txt", "../README.md"]
+    # Define folders and files to add to file tar.gz
+    folders_to_add = ["../pybioportal"]
+    files_to_add = ["../setup.py", "../LICENSE.txt", "../README.md"]
 
-    # Nome del file tar.gz da creare
-    nome_file_output = f"../archive/pybioportal-{vVERSION}.tar.gz"
+    # tar.gz file name
+    output_file = f"../archive/pybioportal-{vVERSION}.tar.gz"
 
-    # Crea il file tar.gz
-    crea_tar_gz(nome_file_output, cartelle_da_aggiungere, file_da_aggiungere)
+    create_tar_gz(output_file, folders_to_add, files_to_add)
