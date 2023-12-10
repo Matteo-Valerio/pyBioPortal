2d3457ae40e0f3a1e4051d86879d7c3519a2a9b0
Matteo Valerio <matteo.valerio@live.it>
2023-12-10 11:34:19 +0100

    added script create git tag version

--- tools/run_process_build.py
+++ tools/run_process_build.py
@@ -4,7 +4,8 @@ import subprocess
 scripts_to_run = ["update_meta_yaml.py", 
                   "create_archive.py", 
                   "run_pypi_build.py", 
-                  "run_conda_build.py"]
+                  "run_conda_build.py",
+                  "create_git_tag_version.py"]
 
 for script in scripts_to_run:
     subprocess.run(["python", script])
