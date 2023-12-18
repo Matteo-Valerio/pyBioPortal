0572f36d930deb53f39e12ed11f5792b51177a0d
Matteo Valerio <matteo.valerio@live.it>
2023-12-18 22:40:16 +0100

    setup commad complete and clean folder added

--- tools/run_pypi_build.py
+++ tools/run_pypi_build.py
@@ -9,9 +9,10 @@ if not os.path.exists(output_folder):
 
 # commands to execute
 commands = [
-    "python setup.py",
+    f"python setup.py sdist --dist-dir {output_folder} bdist_wheel --dist-dir {output_folder}"
 ]
 
+# command must be executed in root folder of repository
 os.chdir("..")
 
 print(f"Working Directory: {os.getcwd()}",)
@@ -19,6 +20,19 @@ print(f"Working Directory: {os.getcwd()}",)
 for cmd in commands:
     print(f">>{cmd}: \n")
     subprocess.run(cmd, shell=True)
+
+# Remove the build and pybioportal.egg-info directory after creating the packages
+if os.path.exists("build"):
+    if os.name == "nt":  # Check if running on Windows
+        os.system(f"rmdir /s /q build")
+    else:  # For Unix-like systems
+        os.system(f"rm -rf build")
+
+if os.path.exists("pybioportal.egg-info"):
+    if os.name == "nt":  # Check if running on Windows
+        os.system(f"rmdir /s /q pybioportal.egg-info")
+    else:  # For Unix-like systems
+        os.system(f"rm -rf pybioportal.egg-info")
 
 # commit pypi build folder 
 os.chdir(output_folder)
