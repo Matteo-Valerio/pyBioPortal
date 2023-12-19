f82222961d28eea9add409551486b8622618e017
Matteo Valerio <matteo.valerio@live.it>
2023-12-19 14:01:28 +0100

    insert update_pyprj_toml

--- tools/run_process_build.py
+++ tools/run_process_build.py
@@ -1,7 +1,8 @@
 import subprocess
 
 # script list to run
-scripts_to_run = ["update_meta_yaml.py", 
+scripts_to_run = ["update_pyprj_toml.py",
+                  "update_meta_yaml.py", 
                   "create_archive.py", 
                   "run_pypi_build.py", 
                   "run_conda_build.py",
